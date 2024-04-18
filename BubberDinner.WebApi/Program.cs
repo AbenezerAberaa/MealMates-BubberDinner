225bde855460fa26bc4ff044538d7b3d6a326c05
Abenezer Abera <abenezer.aberaa@gmail.com>
2024-04-18 12:16:12 +0300

    add basic error handling

--- BubberDinner.WebApi/Program.cs
+++ BubberDinner.WebApi/Program.cs
@@ -1,19 +1,23 @@
 using BubberDinner.Application;
 using BubberDinner.Infrastructure;
+using BubberDinner.WebApi.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
-    builder.Services.AddControllers();
+    builder.Services.AddControllers(options =>
+    {
+        options.Filters.Add<ErrorHandlingFilterAttribute>();
+    });
 }
 
 var app = builder.Build();
 {
+    //app.UseMiddleware<ErrorHandlingMiddleWare>();
     app.UseHttpsRedirection();
     app.MapControllers();
-
     app.Run();
 }
 
